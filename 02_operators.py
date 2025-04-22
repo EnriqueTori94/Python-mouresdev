4aeea14d629f88ab510306de0766ba75cf5561d4
EnriqueTori94 <enriquetoribio105@outlook.com>
2025-04-21 19:45:59 -0600

    Checkpoint
    Subiremos los ficheros de python en los que hemos trabajado hasta el dia de hoy?

---
+++ 02_operators.py
@@ -0,0 +1,18 @@
+#operadores
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(10 / 3)
+print(10 // 3)
+#te genera una potencia
+print(2 ** 3)
+
+###puedes usar el + para concatenar pero no para las demas operaciones
+print("hola" + "python")
+print("hola " + str(5))
+
+#aqui podemos jugar con algunos de los operadores
+print("Hola " * 5)
+my_float = 2.5 *2
+print("hola " * int(my_float))
+print("hola " + str(my_float))
